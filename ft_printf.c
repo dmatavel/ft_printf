2ba7357ef09c96ea1cdcfa0894167e0376c19fb8
dmatavel <matavelidanilo@gmail.com>
2022-07-04 13:21:33 -0300

    rebase everthing

--- ft_printf.c
+++ ft_printf.c
@@ -6,51 +6,15 @@
 /*   By: dmatavel <dmatavel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 16:39:57 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/06/29 14:34:36 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/07/04 13:20:52 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 int	ft_printf(const char *format, ...)
 {
-	t_print *tab;
-
-	tab = (t_print *)malloc(sizeof(t_print));
-	if (!tab)
-		return (-1);
 	int	i;
 	int	ret;
-
-	i = 0;
-	ret = 0;
-	va_start(tab->args, format);
-	while (format[i] != '\0')
-	{
-		if(format[i] == '%')
-			{
-				if (format[i + 1] == 'c')
-				{
-					ft_print_char(tab);
-					i++;
-				}
-				else if (format[i + 1] == 's')
-				{
-					ft_print_str(tab);
-					i++;
-				}
-				else if (format[i + 1] == 'd' || format[i + 1] == 'i')
-				{
-					ft_print_int(tab);
-					i++;
-				}
-			}
-		else
-			tab->tl += write(1, &format[i], 1);
-		i++;
-	}
-	ret = tab->tl;
-	free(tab);
-	return (ret);
+	va_list	args;
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,32 +6,16 @@
 /*   By: dmatavel <dmatavel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 07:11:44 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/06/29 14:32:03 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/07/04 13:09:23 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-/* includes */
 #include "libft/libft.h"
 #include <stdarg.h>
 
-/* type definitions */
-
-typedef struct s_print
-{
-	va_list	args;
-	int		tl;
-	int		perc;
-}	t_print;
-
-/* declarations */
-
-int			ft_printf(const char *format, ...);
-int			ft_eval_format(t_print *tab, const char *format, int index);
-void		ft_print_char(t_print *tab);
-void		ft_print_str(t_print *tab);
-void		ft_print_int(t_print *tab);
+int	ft_printf(const char *format, ...);
 
 #endif
