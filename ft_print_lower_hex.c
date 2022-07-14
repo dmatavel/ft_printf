ad5c52f2dfc912d969bd6a1e9255c83f9e36f7f5
dmatavel <matavelidanilo@gmail.com>
2022-07-14 16:49:40 -0300

    print lower hex tests ok

--- ft_print_lower_hex.c
+++ ft_print_lower_hex.c
@@ -10,91 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-static void	swap(char *x, char *y)
-{
-	char temp;
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
-static char *reverse(char *buffer, int i, int j)
-{
-	while (i < j)
-		swap(&buffer[i++], &buffer[j--]);
-	return (buffer);
-}
-
-static int	ft_abs(int n)
-{
-	if (n < 0)
-		return (-n);
-	else
-		return (n);
-}
-
-static char *itoa_base(int value, char *buffer, int base)
-{
-	int i;
-	int n;
-	int r;
-
-	if (base < 2 || base > 32)
-		return (buffer);
-	n = ft_abs(value);
-
-	i = 0;
-	while (n)
-	{
-		r = n % base;
-		if (r >= 10)
-			buffer[i++] = 65 + (r - 10);
-		else
-			buffer[i++] = 48 + r;
-		n = n / base;
-	}
-	if (i == 0)
-		buffer[i++] = '0';
-	if (value < 0 && base == 10)
-		buffer[i++] = '-';
-	buffer[i] = '\0';
-	reverse(buffer, 0, i - 1i);
-	return (ft_strdup(buffer));
-}
-*/
-
 #include "ft_printf.h"
 
-void	ft_putnbr_hex_fd(unsigned int n, int fd)
+static void	ft_putnbr_hex_fd(unsigned int n, int fd)
 {
-	int r;
-	char ld;
-	char rd;
+	char *radix;
 
+	radix = "0123456789abcdef";
 	if (fd < 0)
 		return ;
-	/*
-	if (n > 15)
+	if (n >= 16)
 		ft_putnbr_hex_fd(n / 16, fd);
-	if (n > 9)
-		ft_putchar_fd((n % 16 - 10) + 97, fd);
+	if (n > 10)
+		ft_putchar_fd(radix[n % 16], fd);
 	else
-		ft_putchar_fd(n % 16 + 48, fd);
-	*/
-	while(n)
-	{
-		r = n % 16;
-		if(r >= 10)
-			rd = 97 + (r - 10);
-		else
-			ld = 48 + r;
-		n = n / 16;
-	}
-	ft_putchar_fd(rd, 1);
-	ft_putchar_fd(ld, 1);
-	if (n == 0)
-		ft_putchar_fd(48, 1);
+		ft_putchar_fd(radix[n % 16], fd);
 }
 
 int	ft_print_lower_hex(unsigned int n)
